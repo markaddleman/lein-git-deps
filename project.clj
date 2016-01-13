6d80d313bbeaff7f7c77a6316b7cbbda147caed8
Duncan McGreggor <oubiwann@gmail.com>
2016-01-13 11:39:45 -0600

    Updated versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-git-deps "0.0.2"
+(defproject lein-git-deps "0.0.3"
   :description "Attach git dependencies to a leiningen project"
   :eval-in-leiningen true
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
