6d700995da04db2553a03072d499ee99d3c1b930
Felipe <fpaolozze@gmail.com>
2017-09-25 18:02:46 -0300

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-git-deps "0.0.3"
+(defproject lein-git-deps "0.0.4"
   :description "Attach git dependencies to a leiningen project"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.7.0"]])
